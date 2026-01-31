fa1fb0de981d008eda02c7b6dde4202de5ff5570
chillcreator <chillcreator@asmrr.ru>
2026-01-31 23:32:14 +0300

    feat: create SettingsForm with 13 tabs and buttons layout
    Added SettingsForm with:
    - TableLayoutPanel for main structure
    - TabControl with 13 tabs (General to Scheduler)
    - Buttons panel with Save and Cancel
    - Form properties set for settings dialog

--- SettingsForm.Designer.cs
+++ SettingsForm.Designer.cs
@@ -28,143 +28,261 @@
         /// </summary>
         private void InitializeComponent()
         {
-            mainTableLayout = new TableLayoutPanel();
-            settingsTabControl = new TabControl();
-            generalTabPage = new TabPage();
-            ytdlpTabPage = new TabPage();
-            networkTabPage = new TabPage();
-            otherTabPage = new TabPage();
-            buttonsPanel = new Panel();
-            saveButton = new Button();
-            cancelButton = new Button();
-            mainTableLayout.SuspendLayout();
-            settingsTabControl.SuspendLayout();
-            buttonsPanel.SuspendLayout();
+            mainTableLayoutPanel = new TableLayoutPanel();
+            tabControlSettings = new TabControl();
+            tabPageGeneral = new TabPage();
+            tabPageOutput = new TabPage();
+            tabPageYtDlp = new TabPage();
+            tabPagePlaylist = new TabPage();
+            tabPageSubtitle = new TabPage();
+            tabPageNetwork = new TabPage();
+            tabPagePostProcessing = new TabPage();
+            tabPageAdvanced = new TabPage();
+            tabPageMonitor = new TabPage();
+            tabPageIntegration = new TabPage();
+            tabPageOther = new TabPage();
+            tabPageProfiles = new TabPage();
+            tabPageScheduler = new TabPage();
+            panelButtons = new Panel();
+            buttonSave = new Button();
+            buttonCancel = new Button();
+            mainTableLayoutPanel.SuspendLayout();
+            tabControlSettings.SuspendLayout();
+            panelButtons.SuspendLayout();
             SuspendLayout();
             // 
-            // mainTableLayout
+            // mainTableLayoutPanel
             // 
-            mainTableLayout.ColumnCount = 1;
-            mainTableLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
-            mainTableLayout.Controls.Add(settingsTabControl, 0, 0);
-            mainTableLayout.Controls.Add(buttonsPanel, 0, 1);
-            mainTableLayout.Dock = DockStyle.Fill;
-            mainTableLayout.Location = new Point(0, 0);
-            mainTableLayout.Name = "mainTableLayout";
-            mainTableLayout.RowCount = 2;
-            mainTableLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
-            mainTableLayout.RowStyles.Add(new RowStyle(SizeType.Absolute, 50F));
-            mainTableLayout.Size = new Size(582, 403);
-            mainTableLayout.TabIndex = 0;
+            mainTableLayoutPanel.ColumnCount = 1;
+            mainTableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            mainTableLayoutPanel.Controls.Add(tabControlSettings, 0, 0);
+            mainTableLayoutPanel.Controls.Add(panelButtons, 0, 1);
+            mainTableLayoutPanel.Dock = DockStyle.Fill;
+            mainTableLayoutPanel.Location = new Point(0, 0);
+            mainTableLayoutPanel.Margin = new Padding(10);
+            mainTableLayoutPanel.Name = "mainTableLayoutPanel";
+            mainTableLayoutPanel.Padding = new Padding(5);
+            mainTableLayoutPanel.RowCount = 2;
+            mainTableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            mainTableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Absolute, 50F));
+            mainTableLayoutPanel.Size = new Size(882, 603);
+            mainTableLayoutPanel.TabIndex = 0;
             // 
-            // settingsTabControl
+            // tabControlSettings
             // 
-            settingsTabControl.Controls.Add(generalTabPage);
-            settingsTabControl.Controls.Add(ytdlpTabPage);
-            settingsTabControl.Controls.Add(networkTabPage);
-            settingsTabControl.Controls.Add(otherTabPage);
-            settingsTabControl.Dock = DockStyle.Fill;
-            settingsTabControl.Location = new Point(3, 3);
-            settingsTabControl.Name = "settingsTabControl";
-            settingsTabControl.SelectedIndex = 0;
-            settingsTabControl.Size = new Size(576, 347);
-            settingsTabControl.TabIndex = 0;
+            tabControlSettings.Controls.Add(tabPageGeneral);
+            tabControlSettings.Controls.Add(tabPageOutput);
+            tabControlSettings.Controls.Add(tabPageYtDlp);
+            tabControlSettings.Controls.Add(tabPagePlaylist);
+            tabControlSettings.Controls.Add(tabPageSubtitle);
+            tabControlSettings.Controls.Add(tabPageNetwork);
+            tabControlSettings.Controls.Add(tabPagePostProcessing);
+            tabControlSettings.Controls.Add(tabPageAdvanced);
+            tabControlSettings.Controls.Add(tabPageMonitor);
+            tabControlSettings.Controls.Add(tabPageIntegration);
+            tabControlSettings.Controls.Add(tabPageOther);
+            tabControlSettings.Controls.Add(tabPageProfiles);
+            tabControlSettings.Controls.Add(tabPageScheduler);
+            tabControlSettings.Dock = DockStyle.Fill;
+            tabControlSettings.Location = new Point(5, 5);
+            tabControlSettings.Margin = new Padding(0);
+            tabControlSettings.Name = "tabControlSettings";
+            tabControlSettings.Padding = new Point(10, 10);
+            tabControlSettings.SelectedIndex = 0;
+            tabControlSettings.Size = new Size(872, 543);
+            tabControlSettings.TabIndex = 0;
             // 
-            // generalTabPage
+            // tabPageGeneral
             // 
-            generalTabPage.Location = new Point(4, 29);
-            generalTabPage.Name = "generalTabPage";
-            generalTabPage.Padding = new Padding(3);
-            generalTabPage.Size = new Size(568, 314);
-            generalTabPage.TabIndex = 0;
-            generalTabPage.Text = "General";
-            generalTabPage.UseVisualStyleBackColor = true;
+            tabPageGeneral.BackColor = Color.White;
+            tabPageGeneral.Location = new Point(4, 43);
+            tabPageGeneral.Name = "tabPageGeneral";
+            tabPageGeneral.Padding = new Padding(3);
+            tabPageGeneral.Size = new Size(864, 496);
+            tabPageGeneral.TabIndex = 0;
+            tabPageGeneral.Text = "General";
             // 
-            // ytdlpTabPage
+            // tabPageOutput
             // 
-            ytdlpTabPage.Location = new Point(4, 29);
-            ytdlpTabPage.Name = "ytdlpTabPage";
-            ytdlpTabPage.Padding = new Padding(3);
-            ytdlpTabPage.Size = new Size(568, 314);
-            ytdlpTabPage.TabIndex = 1;
-            ytdlpTabPage.Text = "yt-dlp";
-            ytdlpTabPage.UseVisualStyleBackColor = true;
+            tabPageOutput.BackColor = Color.White;
+            tabPageOutput.Location = new Point(4, 43);
+            tabPageOutput.Name = "tabPageOutput";
+            tabPageOutput.Padding = new Padding(3);
+            tabPageOutput.Size = new Size(864, 496);
+            tabPageOutput.TabIndex = 1;
+            tabPageOutput.Text = "Output";
             // 
-            // networkTabPage
+            // tabPageYtDlp
             // 
-            networkTabPage.Location = new Point(4, 29);
-            networkTabPage.Name = "networkTabPage";
-            networkTabPage.Size = new Size(568, 314);
-            networkTabPage.TabIndex = 2;
-            networkTabPage.Text = "Network";
-            networkTabPage.UseVisualStyleBackColor = true;
+            tabPageYtDlp.BackColor = Color.White;
+            tabPageYtDlp.Location = new Point(4, 43);
+            tabPageYtDlp.Name = "tabPageYtDlp";
+            tabPageYtDlp.Size = new Size(864, 496);
+            tabPageYtDlp.TabIndex = 2;
+            tabPageYtDlp.Text = "yt-dlp";
             // 
-            // otherTabPage
+            // tabPagePlaylist
             // 
-            otherTabPage.Location = new Point(4, 29);
-            otherTabPage.Name = "otherTabPage";
-            otherTabPage.Size = new Size(568, 314);
-            otherTabPage.TabIndex = 3;
-            otherTabPage.Text = "Other";
-            otherTabPage.UseVisualStyleBackColor = true;
+            tabPagePlaylist.BackColor = Color.White;
+            tabPagePlaylist.Location = new Point(4, 43);
+            tabPagePlaylist.Name = "tabPagePlaylist";
+            tabPagePlaylist.Size = new Size(864, 496);
+            tabPagePlaylist.TabIndex = 3;
+            tabPagePlaylist.Text = "Playlist";
             // 
-            // buttonsPanel
+            // tabPageSubtitle
             // 
-            buttonsPanel.Controls.Add(cancelButton);
-            buttonsPanel.Controls.Add(saveButton);
-            buttonsPanel.Dock = DockStyle.Fill;
-            buttonsPanel.Location = new Point(3, 356);
-            buttonsPanel.Name = "buttonsPanel";
-            buttonsPanel.Size = new Size(576, 44);
-            buttonsPanel.TabIndex = 1;
+            tabPageSubtitle.BackColor = Color.White;
+            tabPageSubtitle.Location = new Point(4, 43);
+            tabPageSubtitle.Name = "tabPageSubtitle";
+            tabPageSubtitle.Size = new Size(864, 496);
+            tabPageSubtitle.TabIndex = 4;
+            tabPageSubtitle.Text = "Subtitle";
             // 
-            // saveButton
+            // tabPageNetwork
             // 
-            saveButton.Location = new Point(400, 10);
-            saveButton.Name = "saveButton";
-            saveButton.Size = new Size(75, 30);
-            saveButton.TabIndex = 0;
-            saveButton.Text = "Save";
-            saveButton.UseVisualStyleBackColor = true;
+            tabPageNetwork.BackColor = Color.White;
+            tabPageNetwork.Location = new Point(4, 43);
+            tabPageNetwork.Name = "tabPageNetwork";
+            tabPageNetwork.Size = new Size(864, 496);
+            tabPageNetwork.TabIndex = 5;
+            tabPageNetwork.Text = "Network";
             // 
-            // cancelButton
+            // tabPagePostProcessing
             // 
-            cancelButton.Location = new Point(485, 10);
-            cancelButton.Name = "cancelButton";
-            cancelButton.Size = new Size(75, 30);
-            cancelButton.TabIndex = 1;
-            cancelButton.Text = "Cancel";
-            cancelButton.UseVisualStyleBackColor = true;
+            tabPagePostProcessing.BackColor = Color.White;
+            tabPagePostProcessing.Location = new Point(4, 43);
+            tabPagePostProcessing.Name = "tabPagePostProcessing";
+            tabPagePostProcessing.Size = new Size(864, 496);
+            tabPagePostProcessing.TabIndex = 6;
+            tabPagePostProcessing.Text = "Post-processing";
+            // 
+            // tabPageAdvanced
+            // 
+            tabPageAdvanced.BackColor = Color.White;
+            tabPageAdvanced.Location = new Point(4, 43);
+            tabPageAdvanced.Name = "tabPageAdvanced";
+            tabPageAdvanced.Size = new Size(864, 496);
+            tabPageAdvanced.TabIndex = 7;
+            tabPageAdvanced.Text = "Advanced";
+            // 
+            // tabPageMonitor
+            // 
+            tabPageMonitor.BackColor = Color.White;
+            tabPageMonitor.Location = new Point(4, 43);
+            tabPageMonitor.Name = "tabPageMonitor";
+            tabPageMonitor.Size = new Size(864, 496);
+            tabPageMonitor.TabIndex = 8;
+            tabPageMonitor.Text = "Monitor";
+            // 
+            // tabPageIntegration
+            // 
+            tabPageIntegration.BackColor = Color.White;
+            tabPageIntegration.Location = new Point(4, 43);
+            tabPageIntegration.Name = "tabPageIntegration";
+            tabPageIntegration.Size = new Size(864, 496);
+            tabPageIntegration.TabIndex = 9;
+            tabPageIntegration.Text = "Integration";
+            // 
+            // tabPageOther
+            // 
+            tabPageOther.BackColor = Color.White;
+            tabPageOther.Location = new Point(4, 43);
+            tabPageOther.Name = "tabPageOther";
+            tabPageOther.Size = new Size(864, 496);
+            tabPageOther.TabIndex = 10;
+            tabPageOther.Text = "Other";
+            // 
+            // tabPageProfiles
+            // 
+            tabPageProfiles.BackColor = Color.White;
+            tabPageProfiles.Location = new Point(4, 43);
+            tabPageProfiles.Name = "tabPageProfiles";
+            tabPageProfiles.Size = new Size(864, 496);
+            tabPageProfiles.TabIndex = 11;
+            tabPageProfiles.Text = "Profiles";
+            // 
+            // tabPageScheduler
+            // 
+            tabPageScheduler.BackColor = Color.White;
+            tabPageScheduler.Location = new Point(4, 43);
+            tabPageScheduler.Name = "tabPageScheduler";
+            tabPageScheduler.Size = new Size(864, 496);
+            tabPageScheduler.TabIndex = 12;
+            tabPageScheduler.Text = "Scheduler";
+            // 
+            // panelButtons
+            // 
+            panelButtons.Controls.Add(buttonCancel);
+            panelButtons.Controls.Add(buttonSave);
+            panelButtons.Dock = DockStyle.Fill;
+            panelButtons.Location = new Point(5, 548);
+            panelButtons.Margin = new Padding(0);
+            panelButtons.Name = "panelButtons";
+            panelButtons.Padding = new Padding(0, 5, 10, 5);
+            panelButtons.Size = new Size(872, 50);
+            panelButtons.TabIndex = 1;
+            // 
+            // buttonSave
+            // 
+            buttonSave.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            buttonSave.Location = new Point(665, 8);
+            buttonSave.Name = "buttonSave";
+            buttonSave.Size = new Size(94, 29);
+            buttonSave.TabIndex = 0;
+            buttonSave.Text = "Save";
+            buttonSave.UseVisualStyleBackColor = true;
+            // 
+            // buttonCancel
+            // 
+            buttonCancel.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            buttonCancel.Location = new Point(765, 8);
+            buttonCancel.Name = "buttonCancel";
+            buttonCancel.Size = new Size(94, 29);
+            buttonCancel.TabIndex = 1;
+            buttonCancel.Text = "Cancel";
+            buttonCancel.UseVisualStyleBackColor = true;
             // 
             // SettingsForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(582, 403);
-            Controls.Add(mainTableLayout);
+            ClientSize = new Size(882, 603);
+            Controls.Add(mainTableLayoutPanel);
             FormBorderStyle = FormBorderStyle.FixedDialog;
             MaximizeBox = false;
             MinimizeBox = false;
+            MinimumSize = new Size(700, 500);
             Name = "SettingsForm";
-            ShowInTaskbar = false;
             StartPosition = FormStartPosition.CenterParent;
-            Text = "Settings";
-            mainTableLayout.ResumeLayout(false);
-            settingsTabControl.ResumeLayout(false);
-            buttonsPanel.ResumeLayout(false);
+            Text = "YTDlpSharp - Settings";
+            mainTableLayoutPanel.ResumeLayout(false);
+            tabControlSettings.ResumeLayout(false);
+            panelButtons.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
 
-        private TableLayoutPanel mainTableLayout;
-        private TabControl settingsTabControl;
-        private TabPage generalTabPage;
-        private TabPage ytdlpTabPage;
-        private TabPage networkTabPage;
-        private TabPage otherTabPage;
-        private Panel buttonsPanel;
-        private Button cancelButton;
-        private Button saveButton;
+        private TableLayoutPanel mainTableLayoutPanel;
+        private TabControl tabControlSettings;
+        private TabPage tabPageGeneral;
+        private TabPage tabPageOutput;
+        private TabPage tabPageYtDlp;
+        private TabPage tabPagePlaylist;
+        private TabPage tabPageSubtitle;
+        private TabPage tabPageNetwork;
+        private TabPage tabPagePostProcessing;
+        private TabPage tabPageAdvanced;
+        private TabPage tabPageMonitor;
+        private TabPage tabPageIntegration;
+        private TabPage tabPageOther;
+        private TabPage tabPageProfiles;
+        private TabPage tabPageScheduler;
+        private Panel panelButtons;
+        private Button buttonCancel;
+        private Button buttonSave;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
     }
 }
